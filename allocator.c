be37a06716e60037ff34b769cb5c6710c6bda896
Tomi <ensotomasgarcia@gmail.com>
2026-05-19 12:46:05 -0300

    corrigiendo el return

--- allocator.c
+++ allocator.c
@@ -17,49 +17,7 @@ const int BLOCK_MARKER = 0xDD;
  * 
  */
 
-int *an_malloc(ssize_t size){
-    if (heap_start == NULL){
-        heap_start = sbrk(0); //the heap position now
-        sbrk(4096); //makes 4096 new bytes for the heap
-    }
-    heap_end = sbrk(0);
-    long int length = heap_end - heap_start;
 
-    // First, we check if the magical bytes are at the beggining of the heap
-
-    if (heap_start != MAGICAL_BYTES){
-    // first execution of malloc
-    
-    *heap_start = MAGICAL_BYTES;
-    my_stats *malloc_header = (my_stats *)heap_start;
-    malloc_header->amount_of_blocks = 1;
-    malloc_header->amount_of_pages = 1;
-    area *fst_block = (area *)((char *)heap_start + sizeof(my_stats));  // the first block occupies all available heap space and will later be split into smaller blocks
-    fst_block->marker = BLOCK_MARKER;
-    fst_block->prev = NULL;
-    fst_block->InUse = false; // false because this is the first block and no one has requested memory yet
-    fst_block->length = length - sizeof(my_stats) - sizeof(area); // we subtract the size of the stats header and the area header from the total heap length 
-    fst_block->next = NULL;
-    }
-}
-
-//[ my_stats | first_block              ]
-//    ^           ^
-// heap_start  heap_start + sizeof(my_stats)
-
-
-
-/**
- * @brief 
- * - *an_malloc: starts the heap and the header (personal doc memory allocator)
- * 
- * why heap_end is a char? its because the char occupies 1 byte 
- * why heap_end ends at the sbrk(0)? because we are writing from the end sbrk(4096) to the beginning sbrk(0)
- * 
- * [  heap_start ——————————→ heap_end  ]
- * 
- * 
- */ 
 
 my_stats *get_malloc_headear(){
     return (my_stats *)heap_start; 
@@ -143,8 +101,53 @@ area *find_last_block(){
     smallest_block->length = size; //now the rest of the spaces is given to the new block
     malloc_header->my_simple_lock = false;
     return (int *)((char *)smallest_block + sizeof(area)); //how we chosee to allocate the memory in the samllest block we return the malloc of the smallest one
-     
+ 
+ }
+
+
+/**
+ * @brief 
+ * - *an_malloc: starts the heap and the header (personal doc memory allocator)
+ * 
+ * why heap_end is a char? its because the char occupies 1 byte 
+ * why heap_end ends at the sbrk(0)? because we are writing from the end sbrk(4096) to the beginning sbrk(0)
+ * 
+ * [  heap_start ——————————→ heap_end  ]
+ * 
+ * 
+ */ 
+
+
+int *an_malloc(ssize_t size){
+    if (heap_start == NULL){
+        heap_start = sbrk(0); //the heap position now
+        sbrk(4096); //makes 4096 new bytes for the heap
+    }
+    heap_end = sbrk(0);
+    long int length = heap_end - heap_start;
+
+    // First, we check if the magical bytes are at the beggining of the heap
+
+    if (heap_start != MAGICAL_BYTES){
+    // first execution of malloc
+    
+    heap_start = MAGICAL_BYTES;
+    my_stats *malloc_header = (my_stats *)heap_start;
+    malloc_header->amount_of_blocks = 1;
+    malloc_header->amount_of_pages = 1;
+    area *fst_block = (area *)((char *)heap_start + sizeof(my_stats));  // the first block occupies all available heap space and will later be split into smaller blocks
+    fst_block->marker = BLOCK_MARKER;
+    fst_block->prev = NULL;
+    fst_block->InUse = false; // false because this is the first block and no one has requested memory yet
+    fst_block->length = length - sizeof(my_stats) - sizeof(area); // we subtract the size of the stats header and the area header from the total heap length 
+    fst_block->next = NULL;
+    }
+    return add_used_block(size);
 }
+
+//[ my_stats | first_block              ]
+//    ^           ^
+// heap_start  heap_start + sizeof(my_stats)
 
 
 //Now we gonna free the memory
